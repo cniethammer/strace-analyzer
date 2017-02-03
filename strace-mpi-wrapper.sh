7bb4dbf877d4718f0723f2a2faec19db56898af3
Christoph Niethammer <niethammer@hlrs.de>
2017-02-03 15:56:55 +0100

    Allow use of a relative path for the application

--- strace-mpi-wrapper.sh
+++ strace-mpi-wrapper.sh
@@ -3,6 +3,8 @@
 # mpirun [MPI OPTIONS] ./strace-mpi-wrapper.sh APP [APP OPTIONS]
 #
 
+cd $PWD
+
 MPI_RANK=${MPI_RANK:=$PMI_RANK}
 MPI_RANK=${MPI_RANK:=$OMPI_COMM_WORLD_RANK}
 MPI_RANK=${MPI_RANK:=$ALPS_APP_PE}
