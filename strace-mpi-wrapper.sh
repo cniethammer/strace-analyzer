a47f037f531e4b8287541faa0d4e00ffeca3e42a
Christoph Niethammer <niethammer@hlrs.de>
2017-02-03 15:37:54 +0100

    Include the MPI rank into the trace file name.

--- strace-mpi-wrapper.sh
+++ strace-mpi-wrapper.sh
@@ -2,5 +2,10 @@
 #
 # mpirun [MPI OPTIONS] ./strace-mpi-wrapper.sh APP [APP OPTIONS]
 #
-LOGFILE="$(hostname).$$.strace"
+
+MPI_RANK=${MPI_RANK:=$PMI_RANK}
+MPI_RANK=${MPI_RANK:=$OMPI_COMM_WORLD_RANK}
+MPI_RANK=${MPI_RANK:=$ALPS_APP_PE}
+
+LOGFILE="${MPI_RANK:+r${MPI_RANK}-}$(hostname).$$.strace"
 strace -r -T -o "$LOGFILE" "$@"
