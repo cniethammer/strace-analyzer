69451f4255149389551ee77e015968f788797810
Christoph Niethammer <niethammer@hlrs.de>
2018-09-16 13:36:43 +0200

    Fix for process_vm_readv and process_vm_writev calls, which copy data between prcesses.

--- strace-io-stats.py
+++ strace-io-stats.py
@@ -331,7 +331,7 @@ def main(argv) :
           filename = open_file_tracker.get_filename(fd)
           file_access_stats[filename]['close_times'].append(float(match.group('close_time')))
           open_file_tracker.register_close(fd)
-        elif "write(" in line or "writev(" in line:
+        elif "write(" in line or "writev(" in line and not "process_vm_writev" in line:
           #logging.debug("WRITE(V)(:")
           match = re.search(r'(?P<difftime>[0-9]+\.[0-9]+) p?writev?\((?P<fd>[0-9]+), ?.*, (?P<size>[0-9]+)\).*= (?P<write_size>-?[0-9]+).*<(?P<write_time>[0-9]+\.[0-9]+)>', line)
           #logging.debug("{0}".format(match.groupdict()))
@@ -342,7 +342,7 @@ def main(argv) :
             file_access_stats[filename]['write_sizes'].append(int(match.group('write_size')))
           else :
             logging.warning("No Open file found for file descriptor {}".format(fd))
-        elif "read(" in line or "readv(" in line:
+        elif "read(" in line or "readv(" in line and not "process_vm_readv" in line:
           #logging.debug("READ(V):")
           match = re.search(r'(?P<difftime>[0-9]+\.[0-9]+) p?readv?\((?P<fd>[0-9]+), ?.*, (?P<size>[0-9]+)\).*= (?P<read_size>-?[0-9]+).*<(?P<read_time>[0-9]+\.[0-9]+)>', line)
           #logging.debug("{0}".format(match.groupdict()))
